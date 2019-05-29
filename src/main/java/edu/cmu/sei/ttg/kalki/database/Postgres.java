c648c5bf92b658e65018d8f67d3c7a74aa8b0aff
cam292 <camazzotta@sei.cmu.edu>
2019-05-29 11:21:26 -0400

    Moved table initialization to a resource file

--- src/main/java/edu/cmu/sei/ttg/kalki/database/Postgres.java
+++ src/main/java/edu/cmu/sei/ttg/kalki/database/Postgres.java
@@ -238,7 +238,8 @@ public class Postgres {
     public static void setupDatabase(){
         logger.info("Setting up database.");
         createHstoreExtension();
-        makeTables();
+        initDB("db-tables.sql");
+//        makeTables();
         insertDefaultDeviceTypes();
         insertDefaultSecurityStates();
         createTriggers();
@@ -297,136 +298,31 @@ public class Postgres {
         executeCommand("DROP TABLE IF EXISTS " + tableName);
     }
 
+
     /**
-     * Create tables for each model.
+     * Reads from the specified file to initialize db
+     * @param fileName the file containing SQL commands to execute
      */
-    public static void makeTables(){
-        logger.info("Making tables.");
+    public static void initDB(String fileName){
+        try {
+            InputStream is = Postgres.class.getResourceAsStream("/"+fileName);
+            Scanner s = new Scanner(is);
 
-        executeCommand("CREATE TABLE IF NOT EXISTS alert_type(" +
-                "id                 serial PRIMARY KEY," +
-                "name               varchar(255) NOT NULL," +
-                "description        varchar(255)," +
-                "source             varchar(255)" +
-                ");");
-
-        executeCommand("CREATE TABLE IF NOT EXISTS device_type(" +
-                "id    serial PRIMARY KEY, " +
-                "name  varchar(255)," +
-                "policy_file    bytea," +
-                "policy_file_name    varchar(255)" +
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS device_group(" +
-                "id    serial PRIMARY KEY, " +
-                "name  varchar(255)" +
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS security_state(" +
-                "id     serial PRIMARY KEY," +
-                "name   varchar(255) NOT NULL" +
-                ");");
-
-        executeCommand("CREATE TABLE IF NOT EXISTS tag(" +
-                "id           serial PRIMARY KEY, " +
-                "name         varchar(255) NOT NULL" +
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS umbox_image(" +
-                "id           serial PRIMARY KEY," +
-                "name         varchar(255) NOT NULL," +
-                "path         varchar(255) NOT NULL" +
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS device(" +
-                "id                     serial PRIMARY KEY," +
-                "name                   varchar(255) NOT NULL," +
-                "description            varchar(255)," +
-                "type_id                int NOT NULL REFERENCES device_type(id)," +
-                "group_id               int," +
-                "ip_address             varchar(255)," +
-                "status_history_size    int NOT NULL," +
-                "sampling_rate          int NOT NULL,"  +
-                "current_state_id       int," +
-                "last_alert_id          int"+
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS device_status(" +
-                "device_id     int NOT NULL REFERENCES device(id)," +
-                "attributes    hstore," +
-                "timestamp     TIMESTAMP," +
-                "id            serial    PRIMARY KEY" +
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS device_security_state(" +
-                "id           serial PRIMARY KEY," +
-                "device_id    int NOT NULL REFERENCES device(id)," +
-                "timestamp    TIMESTAMP," +
-                "state_id     int NOT NULL" +
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS device_tag(" +
-                "device_id    int NOT NULL REFERENCES device(id), " +
-                "tag_id       int NOT NULL REFERENCES tag(id)," +
-                "PRIMARY KEY (device_id, tag_id)" +
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS command_lookup(" +
-                "device_type_id     int NOT NULL REFERENCES device_type(id)," +
-                "state_id           int NOT NULL REFERENCES security_state(id)," +
-                "name               varchar(255)," +
-                "PRIMARY KEY (device_type_id, state_id, name)" +
-                ");");
-
-        executeCommand("CREATE TABLE IF NOT EXISTS umbox_instance(" +
-                "id                 serial PRIMARY KEY, " +
-                "alerter_id         varchar(255) UNIQUE, " +
-                "umbox_image_id     int NOT NULL REFERENCES umbox_image(id)," +
-                "device_id          int NOT NULL REFERENCES device(id), " +
-                "started_at         timestamp NOT NULL DEFAULT now()" +
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS umbox_lookup(" +
-                "state_id           int NOT NULL," +
-                "umbox_image_id     int NOT NULL REFERENCES umbox_image(id)," +
-                "device_type_id     int NOT NULL REFERENCES device_type(id)," +
-                "dag_order          int NOT NULL," +
-                "PRIMARY KEY(state_id, umbox_image_id, device_type_id)" +
-                ");");
-
-        executeCommand("CREATE TABLE IF NOT EXISTS alert(" +
-                "id                 serial PRIMARY KEY," +
-                "name               varchar(255) NOT NULL,"+
-                "timestamp          timestamp NOT NULL DEFAULT now()," +
-                "alert_type_id      int REFERENCES alert_type(id)," +
-                "alerter_id         varchar(255) REFERENCES umbox_instance(alerter_id)," +
-                "device_status_id   int REFERENCES device_status(id)"+
-                ");"
-        );
-
-        executeCommand("CREATE TABLE IF NOT EXISTS alert_condition(" +
-                "id                 serial PRIMARY KEY," +
-                "variables          hstore," +
-                "device_id          int REFERENCES device(id)," +
-                "alert_type_id      int REFERENCES alert_type(id)" +
-                ");");
-
-        executeCommand("CREATE TABLE IF NOT EXISTS alert_type_lookup(" +
-                "alert_type_id      int REFERENCES alert_type(id)," +
-                "device_type_id     int REFERENCES device_type(id)," +
-                "PRIMARY KEY(alert_type_id, device_type_id)" +
-                ");");
-
-
+            String line, statement = "";
+            while(s.hasNextLine()){
+                line = s.nextLine();
+                if(!line.equals("") || !line.equals(" ")){ // skip blank lines
+                    statement += line;
+                    if(line.equals(");")){ // end of SQL statement
+                        executeCommand(statement);
+                        statement = "";
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.severe("Error initializing db:");
+            e.printStackTrace();
+        }
 
     }
 
