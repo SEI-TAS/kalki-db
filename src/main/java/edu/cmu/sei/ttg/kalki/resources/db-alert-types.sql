3cf19405b54d413f1e9ea2deb923082b5be9d8c5
cam292 <camazzotta@sei.cmu.edu>
2019-05-29 16:56:29 -0400

    Added populating alert_type on startup

--- src/main/java/edu/cmu/sei/ttg/kalki/resources/db-alert-types.sql
+++ src/main/java/edu/cmu/sei/ttg/kalki/resources/db-alert-types.sql
@@ -7,3 +7,39 @@ INSERT INTO alert_type(name, description, source) values('device-unavailable', '
 INSERT INTO alert_type(name, description, source) values('max-login-attempts', 'A user attempts to login > X times', 'umbox');
 
 INSERT INTO alert_type(name, description, source) values('state-reset', 'Admin issues state reset through dashboard', 'Dashboard');
+
+INSERT INTO alert_type(name, description, source) values('unts-acceleration', 'unts.acceleration > Z && unts.avgAcceleration > Y', 'Iot Monitor');
+
+INSERT INTO alert_type(name, description, source) values('unts-gyro', 'unts.gyroscope != (X±d), (Y±d), (Z±d)', 'Iot Monitor');
+
+INSERT INTO alert_type(name, description, source) values('unts-gyro-secondary', 'unts.gyroscope > avg(last N) && sampleRate > originalRate', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('unts-magnetometer', 'unts.magnetometer != (X±d), (Y±d), (Z±d)', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('unts-magnetometer-online-low', 'unts.magnetometer > ONLINE±X', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('unts-magnetometer-online-high', 'unts.magnetometer > ONLINE±Y (where Y > X)', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('unts-temperature', 'unts.temp > X OR unts.temp < Y', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('unts-temperature-avg', 'unts.temp >/< avg(last N) ± D', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('unts-temperature-online', 'unts.temp >/< ONLINE ± D', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('dlc-motion-sense', 'DLC.motion && PHLE.on/off = off', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('phle-time-off', 'PHLE.on/off = off && PHLE.time-last-change > T minutes', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('phle-odd-one-out', 'PHLE.on/off = off && PHLE.on/off = ON (∀ PHLE in same group)', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('wemo-current-mw-greater-low', 'wemo.currentmw > X', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('wemo-current-mw-greater-high', 'wemo.currentmw > Y', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('wemo-current-mw-same-group', 'wemo.currentmw </> avg(same type/group)±X', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('wemo-last-change', 'wemo.lastchange > M-minutes && wemo.status == ON', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('wemo-time-on', 'wemo.today_on_time > T', 'IoT Monitor');
+
+INSERT INTO alert_type(name, description, source) values('wemo-today-kwh', 'wemo.today_kwh > K', 'IoT Monitor');
