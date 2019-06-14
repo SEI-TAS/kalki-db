993254e119fa318edfcdc0692c21474e210ad249
ConnorMullaly <connor.f.mullaly@gmail.com>
2019-06-14 14:39:02 -0400

    adding all command lookup methods and functionality as well as modifying startup script to include a device command

--- src/main/java/edu/cmu/sei/ttg/kalki/resources/db-command-lookups.sql
+++ src/main/java/edu/cmu/sei/ttg/kalki/resources/db-command-lookups.sql
@@ -1,1 +1,4 @@
-INSERT INTO command_lookup(device_type_id, state_id, name) values ((SELECT id FROM device_type WHERE name='Udoo Neo'), (SELECT id FROM security_state WHERE name='Normal'), 'turn-off' );
+
+INSERT INTO command(name) values ('turn off');
+
+INSERT INTO command_lookup(device_type_id, state_id, command_id) values ((SELECT id FROM device_type WHERE name='Udoo Neo'), (SELECT id FROM security_state WHERE name='Normal'), (SELECT id FROM command WHERE name='turn off'));
