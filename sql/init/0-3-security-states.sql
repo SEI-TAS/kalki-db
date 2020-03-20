f1fd38d7f28ea4bf184279cc54ffe51bd1499ee3
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-03-20 14:57:26 -0400

    Added by default all transitions and a policy condition pointing to the state-reset alert, which should always be implemented for a device type

--- sql/init/0-3-security-states.sql
+++ sql/init/0-3-security-states.sql
@@ -3,3 +3,15 @@ INSERT INTO security_state(name) values ('Normal');
 INSERT INTO security_state(name) values ('Suspicious');
 
 INSERT INTO security_state(name) values ('Attack');
+
+INSERT INTO state_transition(start_sec_state_id, finish_sec_state_id) values(1, 2);
+
+INSERT INTO state_transition(start_sec_state_id, finish_sec_state_id) values(2, 3);
+
+INSERT INTO state_transition(start_sec_state_id, finish_sec_state_id) values(3, 1);
+
+INSERT INTO state_transition(start_sec_state_id, finish_sec_state_id) values(1, 3);
+
+INSERT INTO state_transition(start_sec_state_id, finish_sec_state_id) values(3, 2);
+
+INSERT INTO state_transition(start_sec_state_id, finish_sec_state_id) values(2, 1);
