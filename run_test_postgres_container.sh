e352c8d1116843b0923a011bc96e0062a431fc92
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-12 12:13:53 -0400

    Better separate creation and running

--- run_test_postgres_container.sh
+++ run_test_postgres_container.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-LOAD_EMPTY_MODE="none"
-source load_device_types.sh
-
-docker build -t kalki/kalki-postgres-test -f Dockerfile.test .
+bash build_test_container.sh
 
 docker container stop kalki-postgres-test
 docker run -p 5433:5432 --rm -d --name kalki-postgres-test kalki/kalki-postgres-test
