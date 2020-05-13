0a91283602cd7a67f3d5c9c7b3cc2d56cb28197c
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-12 23:44:05 -0400

    Making running test db more effective, adding scripts to be used by node-setup as well

--- run_test_postgres_container.sh
+++ run_test_postgres_container.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 bash build_test_container.sh
-
-docker container stop kalki-postgres-test
-docker run -p 5433:5432 --rm -d --name kalki-postgres-test kalki/kalki-postgres-test
+bash prepare_env.sh
