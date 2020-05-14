3fa14b57993f86372dee426110d4526912ecfdbc
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-14 13:14:34 -0400

    trying to avoid issue with stop not ready

--- run_test_postgres_container.sh
+++ run_test_postgres_container.sh
@@ -4,5 +4,5 @@ bash build_test_container.sh
 test_db_name='kalki-postgres-test'
 #[[ $(docker ps -f "name=$test_db_name" --format '{{.Names}}') == $test_db_name ]] || \
 
-docker container stop $test_db_name
+docker container stop $test_db_name && \
 docker run -p 5433:5432 --rm -d --name $test_db_name kalki/$test_db_name
