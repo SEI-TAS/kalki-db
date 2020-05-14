a37a650717bf6022c434e4873340e20fb9e35d2d
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-14 14:38:42 -0400

    Fixing dockerfile issue

--- build_dev_container.sh
+++ build_dev_container.sh
@@ -11,6 +11,6 @@ IFS=':' read PROXY_HOST PROXY_PORT <<<"$(echo ${http_proxy/http:\/\//})"
 echo -en "systemProp.http.proxyHost=${PROXY_HOST}\nsystemProp.http.proxyPort=${PROXY_PORT}\n" >> gradle.properties
 echo -en "systemProp.https.proxyHost=${PROXY_HOST}\nsystemProp.https.proxyPort=${PROXY_PORT}\n" >> gradle.properties
 
-docker build --network=host -t kalki/kalki-db-env -f Dockerfile.build .
+docker build -t kalki/kalki-db-env -f Dockerfile.build .
 
 rm gradle.properties
