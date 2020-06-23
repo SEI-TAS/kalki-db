e0e94ee45d11ad095ee3fe014e8dd9c046d07567
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-06-23 16:10:41 -0400

    Added note to trigger problem loading file handler when the folder doesn't exist.

--- src/main/java/edu/cmu/sei/kalki/db/utils/LoggerSetup.java
+++ src/main/java/edu/cmu/sei/kalki/db/utils/LoggerSetup.java
@@ -2,14 +2,27 @@ package edu.cmu.sei.kalki.db.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class LoggerSetup
 {
+    private static final String DEFAULT_LOGS_FOLDER = "/logs";
+
     public static void setup() {
         try(InputStream loggingConfigFile = LoggerSetup.class.getClassLoader().getResourceAsStream("logging.properties"))
         {
+            // Load config file.
             LogManager.getLogManager().readConfiguration(loggingConfigFile);
+
+            if(Files.notExists(Paths.get(DEFAULT_LOGS_FOLDER))) {
+                // Load handlers to trigger lazy loading exception since folder doesn't exist.
+                Logger rootLogger = LogManager.getLogManager().getLogger("");
+                System.out.println("***NOTE: Ignore FileHandler exception trace, folder for file handler was not found.");
+                rootLogger.getHandlers();
+            }
         }
         catch (final IOException e)
         {
