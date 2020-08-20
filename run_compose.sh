b972e80574e7d82aa76e78d7bb165d61deb973e9
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-08-20 15:42:14 -0400

    Undo

--- run_compose.sh
+++ run_compose.sh
@@ -2,8 +2,7 @@
 
 if [ "$1" == "--reset" ]; then
   echo "Reset requested, removing existing DB volume if any..."
-  VOLUME=$(docker container inspect kalki-postgres | grep -o -P '(?<=Name": ").*(?<=kalki-pgdata)')
-  docker volume rm $VOLUME
+  docker volume rm kalki-pgdata
 fi
 
 export HOST_TZ=$(cat /etc/timezone)
