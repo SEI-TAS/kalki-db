61fbc2548fe79ae57727c1493b04885865e2e1e0
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-08-28 15:53:05 -0400

    Fixed flag to skip tests

--- build_dev_container.sh
+++ build_dev_container.sh
@@ -16,6 +16,6 @@ if [ "$1" == "--skip_tests" ]; then
   SKIP_TESTS_ARG=" -x test "
 fi
 
-docker build --network=host --build-arg SKIP_TESTS=${SKIP_TESTS_ARG} -t kalki/kalki-db-env -f Dockerfile.dev .
+docker build --network=host --build-arg SKIP_TESTS="${SKIP_TESTS_ARG}" -t kalki/kalki-db-env -f Dockerfile.dev .
 
 rm gradle.properties
