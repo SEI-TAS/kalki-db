5e511a9b5b120ca233270f850b169da53b714ac2
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-08-31 14:14:18 -0400

    Added scripts for woking with test container

--- run_test_postgres_container.sh
+++ run_test_postgres_container.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 test_db_name='kalki-postgres-test'
-#[[ $(docker ps -f "name=$test_db_name" --format '{{.Names}}') == $test_db_name ]] || \
-
 docker container stop $test_db_name
 sleep 1
 docker run -p 5433:5432 --rm -d --name $test_db_name kalki/$test_db_name
