54fac20126a39d3d7a611842b712bff0b4de133e
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-09-16 17:38:10 -0400

    Fixing param passing

--- build_container.sh
+++ build_container.sh
@@ -4,7 +4,7 @@ LOAD_EMPTY_MODE="all"
 
 # Avoiding issue when passing skip tests param.
 if [ "$1" == "--skip_tests" ]; then
-  source load_device_types.sh
+  bash load_device_types.sh
 else
   source load_device_types.sh "$@"
 fi
